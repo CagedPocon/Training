3e8a1991327738d85b6c29c5b5dfaea9385c5cb8
CagedPocon <pocotan77@gmail.com>
2016-09-14 16:03:08 +0900

    chapter1の修正とignoreの追加

--- javaPractice/Chapter1/ConcatFilePath.java
+++ javaPractice/Chapter1/ConcatFilePath.java
@@ -1,17 +1,17 @@
 public class ConcatFilePath {
 
     public static void main(String[] args) {
-        String concatFile(String folder, String file) {
-            if (!folder.endsWith("\\")) {
-                folder += "\\";
-            }
-            return folder + file;
-        }
         String folder = "folder\\";
         String file = "file";
+
         String s = concatFile(folder, file);
         System.out.println(s);
     }
 
-
+    public static String concatFile(String folder, String file) {
+        if (!folder.endsWith("\\")) {
+            folder += "\\";
+        }
+        return folder + file;
+    }
 }
