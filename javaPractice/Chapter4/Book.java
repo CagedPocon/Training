38b36609ea9a50f927b6b290536112b3eed98ff0
CagedPocon <pocotan77@gmail.com>
2016-09-15 15:22:43 +0900

    chapter4の修正

--- javaPractice/Chapter4/Book.java
+++ javaPractice/Chapter4/Book.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Book implements Comparable<Book>{
+public class Book implements Comparable<Book> ,Cloneable{
     private String title;
     private Date publishDate;
     private String comment;
@@ -10,19 +10,19 @@ public class Book implements Comparable<Book>{
         result = result * 43 + this.publishDate.hashCode();
         return result;
     }
-    public final boolean equals(Object o) {
-        if (o == this) return true;
-        if (o == null) return false;
-        if (!(o instanceof Book)) return false;
-        Book recast = (Book) o;
-        if (!this.title.equals(recast.title)) return false;
-        if (!this.publishDate.equals(recast.publishDate)) return false;
+    public final boolean equals(Object obj) {
+        if (obj == this) {return true;}
+        if (obj == null) {return false;}
+        if (!(obj instanceof Book)) {return false;}
+        Book recastObj = (Book) obj;
+        if (!this.title.equals(recastObj.title)) {return false;}
+        if (!this.publishDate.equals(recastObj.publishDate)) {return false;}
         return true;
     }
     public final int compareTo(Book o) {
-        if (this.publishDate.getTime() < o.publishDate.getTime()) return -1;
-        if (this.publishDate.getTime() == o.publishDate.getTime()) return 0;
-        return 1;
+        if (this.publishDate.getTime() < o.publishDate.getTime()) {return -1;}
+        if (this.publishDate.getTime() > o.publishDate.getTime()) {return 1;}
+        return 0;
     }
     public final void setTitle(String _title) {
         this.title = _title;
@@ -38,9 +38,9 @@ public class Book implements Comparable<Book>{
         return this.publishDate;
     }
     public final Book clone() {
-        Book copying = new Book();
-        copying.title = this.title;
-        copying.publishDate = (Date) this.publishDate.clone();
-        return copying;
+        Book copyingBook = new Book();
+        copyingBook.title = this.title;
+        copyingBook.publishDate = (Date) this.publishDate.clone();
+        return copyingBook;
     }
 }
